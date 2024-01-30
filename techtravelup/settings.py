f1a01cc27a32d96d1491e11dd9ff2e03e558027f
Dileep Kumar Chilakapati <62283863+dileepchil1995@users.noreply.github.com>
2024-01-30 14:19:53 +0530

    Add files via upload

--- techtravelup/settings.py
+++ techtravelup/settings.py
@@ -126,4 +126,4 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = 'https://techtravelup-377d01f6147c.herokuapp.com/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / STATIC_URL
